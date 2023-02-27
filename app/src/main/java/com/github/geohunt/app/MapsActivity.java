3793421059d4394c4bf4010bd51c92b8df5d0be8
Alban Trincherini <alban.trincherini@epfl.ch>
2023-02-27 20:13:57 +0100

    Add basic map implementation

---
+++ app/src/main/java/com/github/geohunt/app/MapsActivity.java
@@ -0,0 +1,83 @@
+package com.github.geohunt.app;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.github.geohunt.app.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker at EPFL
+        LatLng epfl = new LatLng(46.520536, 6.568318);
+        BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.mipmap.treasure_trove_foreground);
+        mMap.addMarker(new MarkerOptions()
+                .position(epfl).title("EPFL")
+                .icon(icon)
+                .draggable(true));
+
+        //Make clicks on markers show the coordinates of the marker
+        googleMap.setOnMarkerClickListener(marker -> {
+            LatLng position = marker.getPosition();
+            Toast.makeText(this, position.toString(), Toast.LENGTH_SHORT).show();
+            return false;
+        });
+
+        //Has to be added for the markers position to get updated
+        googleMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDrag(@NonNull Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(@NonNull Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragStart(@NonNull Marker marker) {
+
+            }
+        });
+    }
+}
