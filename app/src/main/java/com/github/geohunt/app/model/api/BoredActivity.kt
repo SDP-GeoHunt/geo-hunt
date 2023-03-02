d1d8187763c4e946a45ec383669b904feeab96c1
dhalilov <dario.halilovic@epfl.ch>
2023-03-02 15:11:14 +0100

    Add BoredActivity Entity annotation

--- app/src/main/java/com/github/geohunt/app/model/api/BoredActivity.kt
+++ app/src/main/java/com/github/geohunt/app/model/api/BoredActivity.kt
@@ -1,11 +1,13 @@
 package com.github.geohunt.app.model.api
 
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 /**
  * Represents the JSON response from the Bored API.
  */
+@Entity
 data class BoredActivity(
     @ColumnInfo(name = "activity") val activity: String,
     val type: String,
