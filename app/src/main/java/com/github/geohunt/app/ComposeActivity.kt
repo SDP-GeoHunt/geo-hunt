6d6354e7c0b6597c1d42fed456e5dec21ecb62a8
Nicolas S <nicoszw1@outlook.com>
2023-03-07 01:05:08 +0100

    Provide basic implementation of Google Maps

--- app/src/main/java/com/github/geohunt/app/ComposeActivity.kt
+++ app/src/main/java/com/github/geohunt/app/ComposeActivity.kt
@@ -1,28 +1,16 @@
 package com.github.geohunt.app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.FragmentActivity
 import com.github.geohunt.app.ui.theme.GeoHuntTheme
 
-class ComposeActivity : ComponentActivity() {
+class ComposeActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            GeoHuntTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_compose)
     }
 }
 
