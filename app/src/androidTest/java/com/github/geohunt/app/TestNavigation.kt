b4d35d9e79d45d3dd5d1f2b89a11c059fe68b9f4
Marwan <marwanazuz@gmail.com>
2023-03-09 17:53:38 +0100

    Add navigation quick test

--- app/src/androidTest/java/com/github/geohunt/app/TestNavigation.kt
+++ app/src/androidTest/java/com/github/geohunt/app/TestNavigation.kt
@@ -1,17 +1,14 @@
 package com.github.geohunt.app
 
-import androidx.activity.ComponentActivity
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.test.*
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.navigation.NavDestination
-import androidx.navigation.NavDestinationBuilder
-import androidx.navigation.NavGraphBuilder
+import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.ComposeNavigator
-import androidx.navigation.createGraph
 import androidx.navigation.testing.TestNavHostController
 import com.github.geohunt.app.ui.components.NavigationBar
+import com.github.geohunt.app.ui.components.NavigationController
 import com.github.geohunt.app.ui.components.Route
 import org.junit.Before
 import org.junit.Rule
@@ -20,35 +17,41 @@ import org.junit.Test
 class TestNavigation {
 
     @get:Rule
-    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    val composeTestRule = createComposeRule()
 
     private lateinit var navController: TestNavHostController
+
     @Before
-    fun setupNavHost() {
+    fun setUp() {
         composeTestRule.setContent {
             navController = TestNavHostController(LocalContext.current)
-            val navigator = ComposeNavigator()
-            navController.navigatorProvider.addNavigator(navigator)
-
-            navController.createGraph(Route.Home.route, builder = {
-                for (route in Route.values()) this.addDestination(NavDestinationBuilder(navigator, route.route).build())
-                println("OKKKKKK GOOOD")
-            })
-
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            NavigationController(navController = navController)
             NavigationBar(navController = navController)
         }
     }
 
     @Test
-    fun correctIconIsActive() {
-        println("BEGINNNGJSDHNFDS")
+    fun startRouteIsHome() {
+        assert(Route.Home.route == navController.currentBackStackEntry?.destination?.route)
+    }
+
+    @Test
+    fun clickingOnButtonSelectsIt() {
         for (route in Route.values()) {
-            navController.navigate(route.route)
-            composeTestRule.onNode(hasTestTag("navbtn-" + route.route)).assertIsSelected()
-            // check all other buttons that they are disabled
-            for(route2 in Route.values()) {
-                if (route != route2) composeTestRule.onNode(hasTestTag("navbtn-" + route2.route)).assertIsNotSelected()
-            }
+            val node = composeTestRule.onNode(hasTestTag("navbtn-" + route.route))
+            node.performClick()
+            node.assertIsSelected()
+
+        }
+    }
+
+    @Test
+    fun clickingOnButtonRedirects() {
+        for (route in Route.values()) {
+            val node = composeTestRule.onNode(hasTestTag("navbtn-" + route.route))
+            node.performClick()
+            assert(navController.currentBackStackEntry?.destination?.route == route.route)
         }
     }
 }
