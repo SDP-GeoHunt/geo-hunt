05d9ba2cde56e389bd07906e3d6728418b04031a
Alban Trincherini <alban.trincherini@epfl.ch>
2023-03-15 23:37:42 +0100

    Adapt to API changes and remove preview

--- app/src/main/java/com/github/geohunt/app/Challenge.kt
+++ app/src/main/java/com/github/geohunt/app/Challenge.kt
@@ -1,6 +1,5 @@
 package com.github.geohunt.app
 
-import android.graphics.Bitmap
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
@@ -21,15 +20,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.github.geohunt.app.model.LazyRef
 import com.github.geohunt.app.model.database.api.Challenge
-import com.github.geohunt.app.model.database.api.Location
-import com.github.geohunt.app.model.database.api.PictureImage
-import com.github.geohunt.app.ui.theme.GeoHuntTheme
+import com.github.geohunt.app.model.database.api.Claim
 import com.github.geohunt.app.utility.DateFormatUtils
-import java.time.LocalDateTime
-import java.util.concurrent.CompletableFuture
 
 /**
  * Composable function that displays a challenge given as an argument
@@ -79,14 +74,14 @@ fun ClaimButton() {
 
 @Composable
 fun ChallengeInformation(challenge: Challenge) {
-    val published = challenge.published
+    val published = challenge.publishedDate
     val expirationDate = challenge.expirationDate
 
     Column(modifier = Modifier
             .fillMaxWidth()
             .padding(5.dp, 0.dp)) {
 
-        Text(text = stringResource(id = R.string.challenge_created_by, challenge.uid),
+        Text(text = stringResource(id = R.string.challenge_created_by, challenge.author),
                 color = Color.Gray)
 
         Row(modifier = Modifier.fillMaxWidth(),
@@ -103,14 +98,14 @@ fun GrayText(text: String) {
 }
 
 @Composable
-fun ClaimPictures(claims: List<String>) {
+fun ClaimPictures(claims: List<LazyRef<Claim>>) {
     LazyVerticalGrid(columns = GridCells.Fixed(3)) {
-        items(claims) {cid ->
+        items(claims) {claim ->
             //placeholder, will be replaced by images of claims made to the challenge
             Box(modifier = Modifier
                     .border(BorderStroke(1.dp, Color.Red))
                     .aspectRatio(1f)) {
-                Text(text = cid, color = Color.Red, textAlign = TextAlign.Center, modifier = Modifier.fillMaxSize())
+                Text(text = claim.id, color = Color.Red, textAlign = TextAlign.Center, modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -121,43 +116,4 @@ fun ClaimPictures(claims: List<String>) {
 fun submitPosition() {
     //Todo use utility function to get Localisation and create Claim
     return
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview2() {
-    val dummyChallenge = object : Challenge {
-        override val cid: String
-            get() = "1234"
-        override val uid: String
-            get() = "5678"
-        override val published: LocalDateTime
-            get() = LocalDateTime.of(2010, 7, 24, 20, 54)
-        override val expirationDate: LocalDateTime?
-            get() = LocalDateTime.of(2024, 3, 8, 18, 12)
-        override val thumbnail: PictureImage
-            get() = object : PictureImage {
-                override val iid: String
-                    get() = "4321"
-                override val bitmap: Bitmap?
-                    get() = null
-
-                override fun load(): CompletableFuture<Bitmap> {
-                    return CompletableFuture.completedFuture(bitmap)
-                }
-                override fun save(): CompletableFuture<Void> {
-                    TODO("Not yet implemented")
-                }
-            }
-        override val coarseLocation: Location
-            get() = TODO("Not yet implemented")
-        override val correctLocation: Location
-            get() = TODO("Not yet implemented")
-        override val claims: List<String>
-            get() = (1..100).toList().map { i -> i.toString() }
-    }
-    GeoHuntTheme {
-        Challenge(challenge = dummyChallenge)
-    }
 }
