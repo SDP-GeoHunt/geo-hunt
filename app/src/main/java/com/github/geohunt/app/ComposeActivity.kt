79c9c20c66329fe08e42876a760765142d81354c
Kishlay Chhajer <kishlaychhajer17@gmail.com>
2023-03-16 05:06:40 +0100

    Removed comment

--- app/src/main/java/com/github/geohunt/app/ComposeActivity.kt
+++ app/src/main/java/com/github/geohunt/app/ComposeActivity.kt
@@ -20,7 +20,6 @@ class ComposeActivity : ComponentActivity() {
             GeoHuntTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-//                    HomeScreen().FeedScreen()
                     Greeting("Android")
                 }
             }
