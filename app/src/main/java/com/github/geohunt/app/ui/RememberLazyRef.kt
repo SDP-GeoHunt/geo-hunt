e2bee1f5eef19b0034717b84ef30e645daa5a550
Marwan <marwanazuz@gmail.com>
2023-03-17 15:44:21 +0100

    Fix authentication dependencies

--- app/src/main/java/com/github/geohunt/app/ui/RememberLazyRef.kt
+++ app/src/main/java/com/github/geohunt/app/ui/RememberLazyRef.kt
@@ -4,8 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.databinding.Observable
-import androidx.databinding.Observable.OnPropertyChangedCallback
 import com.github.geohunt.app.model.LazyRef
 
 /**
