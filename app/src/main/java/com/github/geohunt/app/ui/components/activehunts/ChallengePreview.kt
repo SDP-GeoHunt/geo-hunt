2f10cb3f4dbc7fe2b8e47c275b575d3aab03e7f4
Alban Trincherini <alban.trincherini@epfl.ch>
2023-03-22 23:57:34 +0100

    Add ChallengePreview structure

---
+++ app/src/main/java/com/github/geohunt/app/ui/components/activehunts/ChallengePreview.kt
@@ -0,0 +1,10 @@
+package com.github.geohunt.app.ui.components.activehunts
+
+import androidx.compose.runtime.Composable
+import com.github.geohunt.app.model.LazyRef
+import com.github.geohunt.app.model.database.api.Challenge
+
+@Composable
+fun ChallengePreview(challenge: LazyRef<Challenge>) {
+
+}
