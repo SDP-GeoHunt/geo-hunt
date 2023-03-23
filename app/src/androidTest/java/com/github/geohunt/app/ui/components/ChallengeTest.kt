d84160be357fbb1723c0ca3aea66a92ffa6d3720
Alban Trincherini <alban.trincherini@epfl.ch>
2023-03-23 23:40:56 +0100

    Optimize ChallengeTest.kt imports

--- app/src/androidTest/java/com/github/geohunt/app/ui/components/ChallengeTest.kt
+++ app/src/androidTest/java/com/github/geohunt/app/ui/components/ChallengeTest.kt
@@ -1,28 +1,17 @@
 package com.github.geohunt.app.ui.components
 
-import android.util.Log
 import androidx.compose.material.Text
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.databinding.Observable
-import androidx.databinding.Observable.OnPropertyChangedCallback
-import com.github.geohunt.app.mocks.MockLazyRef
-import com.github.geohunt.app.model.LazyRef
 import com.github.geohunt.app.model.database.FirebaseEmulator
-import com.github.geohunt.app.model.database.api.Challenge
-import com.github.geohunt.app.model.database.api.Claim
 import com.github.geohunt.app.model.database.firebase.FirebaseDatabase
 import com.github.geohunt.app.ui.rememberLazyRef
 import com.github.geohunt.app.ui.theme.GeoHuntTheme
 import com.github.geohunt.app.utility.findActivity
 import org.junit.Before
-import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
-import java.util.concurrent.CompletableFuture
 
 class ChallengeTest {
     private lateinit var database: FirebaseDatabase
