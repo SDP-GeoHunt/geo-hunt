3fa97523e951d79c3ef52fbefb6e2d7c616c7a33
Alban Trincherini <alban.trincherini@epfl.ch>
2023-03-25 20:47:06 +0100

    Integrate FetchComponent into code

--- app/src/main/java/com/github/geohunt/app/ui/components/activehunts/ChallengePreview.kt
+++ app/src/main/java/com/github/geohunt/app/ui/components/activehunts/ChallengePreview.kt
@@ -3,44 +3,32 @@ package com.github.geohunt.app.ui.components.activehunts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import com.github.geohunt.app.model.LazyRef
 import com.github.geohunt.app.model.database.api.Challenge
-import com.github.geohunt.app.ui.rememberLazyRef
+import com.github.geohunt.app.ui.FetchComponent
 
 @Composable
 fun ChallengePreview(challenge: LazyRef<Challenge>) {
-    val waitingChallenge = rememberLazyRef { challenge }
 
-    if(waitingChallenge.value == null) {
-        CircularProgressIndicator()
-    }
-    else {
+    FetchComponent(lazyRef = { challenge }) { resolvedChallenge ->
         Column(modifier = Modifier.fillMaxSize()) {
-            val resolvedChallenge = waitingChallenge.value!!
-
             ChallengeImage(challenge = resolvedChallenge)
 
             ChallengeDescription(challenge = resolvedChallenge)
         }
     }
+
 }
 
 @Composable
 fun ChallengeImage(challenge: Challenge) {
     val thumbnail = challenge.thumbnail
-    val waitingThumbnail = rememberLazyRef { thumbnail }
 
-    if(waitingThumbnail.value == null) {
-        CircularProgressIndicator()
-    }
-    else {
-        val resolvedThumbnail = waitingThumbnail.value!!
-
+    FetchComponent(lazyRef = { thumbnail }) {resolvedThumbnail ->
         Image(painter = BitmapPainter(resolvedThumbnail.asImageBitmap()),
                 contentDescription = "Challenge ${challenge.cid}")
     }
