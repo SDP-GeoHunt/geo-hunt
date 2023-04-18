4247a100c09be4c236cd672b3e7502bd13cf7a7d
dhalilov <dario.halilovic@epfl.ch>
2023-04-18 23:12:52 +0200

    Create network monitor and bind main activity to view model

---
+++ app/src/main/java/com/github/geohunt/app/ui/MainViewModel.kt
@@ -0,0 +1,38 @@
+package com.github.geohunt.app.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.geohunt.app.model.database.NetworkMonitor
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+/**
+ * Top-level [androidx.lifecycle.ViewModel] of the app.
+ *
+ * This view model includes general information about the state of the app, such as network connectivity.
+ *
+ * For specific screen states, new view models should be created and bound to the navigation entries'
+ * top-level composables in [com.github.geohunt.app.ui.components.navigation.NavigationController].
+ */
+class MainViewModel : ViewModel() {
+    private val networkMonitor = NetworkMonitor(Firebase.database)
+    private var _isConnected = MutableStateFlow(true)
+
+    /**
+     * Returns true if the application is connected to the remote database.
+     */
+    val isConnected: StateFlow<Boolean> = _isConnected.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            // Bind the UI state and the network monitor
+            networkMonitor.isConnected.collect {
+                _isConnected.value = it
+            }
+        }
+    }
+}
