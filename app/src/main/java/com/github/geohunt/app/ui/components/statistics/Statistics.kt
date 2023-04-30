98fa66cbd9f29662477d445a080e494032a11716
Alban Trincherini <alban.trincherini@epfl.ch>
2023-04-30 18:43:10 +0200

    Add empty screen when no claim

--- app/src/main/java/com/github/geohunt/app/ui/components/statistics/Statistics.kt
+++ app/src/main/java/com/github/geohunt/app/ui/components/statistics/Statistics.kt
@@ -1,11 +1,13 @@
 package com.github.geohunt.app.ui.components.statistics
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
@@ -16,9 +18,27 @@ import com.github.geohunt.app.ui.theme.Lobster
 
 @Composable
 fun Statistics(claims: List<Claim>) {
+    if(claims.size < 2) {
+        EmptyStatisticsScreen()
+    }
+    else {
+        DisplayStatistics(claims = claims)
+    }
+}
+
+@Composable
+fun EmptyStatisticsScreen() {
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+        Text(text = stringResource(id = R.string.statistics_empty_screen_text))
+    }
+}
+
+@Composable
+fun DisplayStatistics(claims: List<Claim>) {
     val dateGranularityState = remember { mutableStateOf(DateGranularity.WEEK) }
 
-    Row(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize()) {
         StatisticsTitle()
 
         ListDropdownMenu(state = dateGranularityState,
