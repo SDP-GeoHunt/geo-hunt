5aec14e91548baf9e8e0503525c84bc7ebf4deb0
Alban Trincherini <alban.trincherini@epfl.ch>
2023-04-30 17:00:32 +0200

    Remove preview

--- app/src/main/java/com/github/geohunt/app/ui/components/statistics/Statistics.kt
+++ app/src/main/java/com/github/geohunt/app/ui/components/statistics/Statistics.kt
@@ -8,13 +8,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.github.geohunt.app.R
 import com.github.geohunt.app.model.database.api.Claim
 import com.github.geohunt.app.ui.components.utils.ListDropdownMenu
 import com.github.geohunt.app.ui.theme.Lobster
-import java.time.LocalDate
 
 @Composable
 fun Statistics(claims: List<Claim>) {
@@ -36,13 +34,4 @@ fun StatisticsTitle() {
     Text(text = stringResource(id = R.string.statistics_title),
             fontFamily = Lobster,
             fontSize = 25.sp)
-}
-
-@Preview
-@Composable
-fun Test() {
-    val now = LocalDate.now()
-    val range = listOf<Long>(0, 3, 6, 9, 12, 15, 18)
-    val dates = (0 until 7).reversed().map { now.minusDays(it.toLong()) }
-    DateGraph(xDateValues = dates, DateGranularity.WEEK, yValues = range.toList().map { it*it })
 }
