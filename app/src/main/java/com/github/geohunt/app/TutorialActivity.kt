d67ee2c02d105c0c2243103da7cf2df01daf17b9
Nicolas <102697940+Nicoslz@users.noreply.github.com>
2023-05-02 14:51:10 +0200

    Skip displaying tutorial on too small screens

--- app/src/main/java/com/github/geohunt/app/TutorialActivity.kt
+++ app/src/main/java/com/github/geohunt/app/TutorialActivity.kt
@@ -1,5 +1,6 @@
 package com.github.geohunt.app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,9 +10,12 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import com.github.geohunt.app.ui.theme.GeoHuntTheme
+import androidx.compose.ui.platform.LocalConfiguration
 import com.github.geohunt.app.ui.components.tutorial.Tutorial
 import com.github.geohunt.app.ui.components.tutorial.WelcomeScreen
+import com.github.geohunt.app.ui.theme.GeoHuntTheme
+import com.github.geohunt.app.utility.replaceActivity
+import kotlin.math.roundToInt
 
 /**
  * The activity that handles display of the tutorial
@@ -25,10 +29,21 @@ class TutorialActivity : ComponentActivity()  {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
-                )
-                {
+                ) {
                     val activity = this@TutorialActivity
                     val shouldShowTutorial = remember { mutableStateOf(false) }
+
+                    val configuration = LocalConfiguration.current
+                    val screenDensity = configuration.densityDpi / 160f
+                    val screenHeightPx =
+                        (configuration.screenHeightDp.toFloat() * screenDensity).roundToInt()
+                    val screenWidthPx =
+                        (configuration.screenWidthDp.toFloat() * screenDensity).roundToInt()
+
+                    // If the screen is too small, skip displaying the welcome screen and tutorial
+                    if (screenHeightPx < 800 || screenWidthPx < 480) {
+                        activity.replaceActivity(Intent(activity, LoginActivity::class.java))
+                    }
 
                     if (shouldShowTutorial.value) {
                         Tutorial(activity)
