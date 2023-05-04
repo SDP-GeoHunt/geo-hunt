192d517590651b712acfe921963f4640b1629c9f
BoyeGuillaume <guillaume.boye@epfl.ch>
2023-05-04 19:21:04 +0200

    arogubpergu

--- app/src/test/java/com/github/geohunt/app/ui/components/profile/edit/EditedUserTest.kt
+++ app/src/test/java/com/github/geohunt/app/ui/components/profile/edit/EditedUserTest.kt
@@ -1,14 +1,15 @@
 package com.github.geohunt.app.ui.components.profile.edit
 
-import com.github.geohunt.app.mocks.MockUser
+import com.github.geohunt.app.model.EditedUser
+import com.github.geohunt.app.model.User
 import org.junit.Test
 
 class EditedUserTest {
 
     @Test
     fun properlyTakesNameFromUser() {
-        val user = MockUser(displayName = "hello")
+        val user = User(id = "zef", displayName = "hello", profilePictureUrl = "")
         val editedUser = EditedUser.fromUser(user)
-        assert(editedUser.displayName == "hello")
+        assert(editedUser.newDisplayName == "hello")
     }
 }
