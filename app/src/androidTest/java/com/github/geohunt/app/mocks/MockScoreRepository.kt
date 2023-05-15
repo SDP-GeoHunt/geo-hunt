07fd4a79e4159919aa69478b723f8c173e4108c4
Alban Trincherini <alban.trincherini@epfl.ch>
2023-05-15 18:52:46 +0200

    Adapt ProfilePage tests

---
+++ app/src/androidTest/java/com/github/geohunt/app/mocks/MockScoreRepository.kt
@@ -0,0 +1,18 @@
+package com.github.geohunt.app.mocks
+
+import com.github.geohunt.app.data.repository.ScoreRepositoryInterface
+import com.github.geohunt.app.model.User
+
+open class MockScoreRepository: ScoreRepositoryInterface {
+    override suspend fun getScore(uid: String): Long {
+        return 0L
+    }
+
+    override suspend fun getTopNUsers(n: Int): List<String> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun incrementUserScore(user: User, increment: Long) {
+        TODO("Not yet implemented")
+    }
+}
