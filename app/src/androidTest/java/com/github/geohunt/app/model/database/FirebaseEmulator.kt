b45371ab92c33f9f12778e9a582ff84595cc3438
Alban Trincherini <alban.trincherini@epfl.ch>
2023-05-25 00:30:54 +0200

    Revert break everything

--- app/src/androidTest/java/com/github/geohunt/app/model/database/FirebaseEmulator.kt
+++ app/src/androidTest/java/com/github/geohunt/app/model/database/FirebaseEmulator.kt
@@ -1,19 +1,12 @@
 package com.github.geohunt.app.model.database
 
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 
 object FirebaseEmulator {
     fun getEmulatedFirebase(): FirebaseDatabase {
-        val instance = Firebase.database
-        try {
-            instance.useEmulator("10.0.2.2", 9000)
-        } catch (_: IllegalStateException) {
+        return FirebaseDatabase.getInstance("http://10.0.2.2:9000/?ns=geohunt-1-default-rtdb")
 
-        }
-        return instance
     }
 
     fun getEmulatedStorage() : FirebaseStorage {
